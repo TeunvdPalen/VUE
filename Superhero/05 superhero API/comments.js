af9851687e3f5564e0a2f8da966eec4c4546d434
Teun vd Palen <teun.vd.palen@gmail.com>
2022-04-26 08:40:05 +0200

    superhero API

--- Superhero/05 superhero API/comments.js
+++ Superhero/05 superhero API/comments.js
@@ -1,22 +1,27 @@
 const comments = {
 	template: '#commentsTemplate',
 	data: function () {
-			return {
-				comments: null,
-				error: false,
-				loading: true,
-			}
+		return {
+			comments: null,
+			error: false,
+			loading: true,
+		};
 	},
 	created() {
-		axios.get("https://jsonplaceholder.typicode.com/comments")
-		.then((response) => {
-				this.comments = response.data.slice(0, 9);
+		axios
+			.get('https://jsonplaceholder.typicode.com/comments', {
+				params: {
+					_limit: 10,
+				},
 			})
-			.catch((error) => {
+			.then(response => {
+				this.comments = response.data;
+			})
+			.catch(error => {
 				console.log(error);
 				// fout met de API request
 				this.errored = true;
 			})
 			.finally(() => (this.loading = false));
-	}
-}
+	},
+};
